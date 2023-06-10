6865c2df9b7dfb11ca39e2e6777a6e77e65e8c06
Tyogrge <2568452755@qq.com>
2023-06-11 00:51:29 +0800

    删除不必要的文件，优化目录，优化静态资源

--- components.d.ts
+++ components.d.ts
@@ -5,39 +5,43 @@
 // Read more: https://github.com/vuejs/core/pull/3399
 import '@vue/runtime-core'
 
-export {}
+export {};
 
-declare module '@vue/runtime-core' {
+declare module "@vue/runtime-core" {
   export interface GlobalComponents {
-    ElAside: typeof import('element-plus/es')['ElAside']
-    ElButton: typeof import('element-plus/es')['ElButton']
-    ElCard: typeof import('element-plus/es')['ElCard']
-    ElCol: typeof import('element-plus/es')['ElCol']
-    ElContainer: typeof import('element-plus/es')['ElContainer']
-    ElHeader: typeof import('element-plus/es')['ElHeader']
-    ElImage: typeof import('element-plus/es')['ElImage']
-    ElInput: typeof import('element-plus/es')['ElInput']
-    ElMain: typeof import('element-plus/es')['ElMain']
-    ElMenu: typeof import('element-plus/es')['ElMenu']
-    ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
-    ElPagination: typeof import('element-plus/es')['ElPagination']
-    ElProgress: typeof import('element-plus/es')['ElProgress']
-    ElRow: typeof import('element-plus/es')['ElRow']
-    ElScrollbar: typeof import('element-plus/es')['ElScrollbar']
-    ElSkeleton: typeof import('element-plus/es')['ElSkeleton']
-    ElSkeletonItem: typeof import('element-plus/es')['ElSkeletonItem']
-    ElTooltip: typeof import('element-plus/es')['ElTooltip']
-    GameDetail: typeof import('./src/components/recordList/gameDetail/index.vue')['default']
-    GmailItem: typeof import('./src/components/recordList/gameList/gmailItem.vue')['default']
-    Pagination: typeof import('./src/components/pagination/index.vue')['default']
-    PlayerItem: typeof import('./src/components/recordList/gameDetail/playerItem.vue')['default']
-    RecordList: typeof import('./src/components/recordList/index.vue')['default']
-    RouterLink: typeof import('vue-router')['RouterLink']
-    RouterView: typeof import('vue-router')['RouterView']
-    SummonerInfo: typeof import('./src/components/summonerInfo/index.vue')['default']
-    SvgIcon: typeof import('./src/components/svgIcon/index.vue')['default']
+    ElAside: typeof import("element-plus/es")["ElAside"];
+    ElButton: typeof import("element-plus/es")["ElButton"];
+    ElCard: typeof import("element-plus/es")["ElCard"];
+    ElCol: typeof import("element-plus/es")["ElCol"];
+    ElContainer: typeof import("element-plus/es")["ElContainer"];
+    ElHeader: typeof import("element-plus/es")["ElHeader"];
+    ElImage: typeof import("element-plus/es")["ElImage"];
+    ElInput: typeof import("element-plus/es")["ElInput"];
+    ElMain: typeof import("element-plus/es")["ElMain"];
+    ElMenu: typeof import("element-plus/es")["ElMenu"];
+    ElMenuItem: typeof import("element-plus/es")["ElMenuItem"];
+    ElPagination: typeof import("element-plus/es")["ElPagination"];
+    ElProgress: typeof import("element-plus/es")["ElProgress"];
+    ElRow: typeof import("element-plus/es")["ElRow"];
+    ElScrollbar: typeof import("element-plus/es")["ElScrollbar"];
+    ElSkeleton: typeof import("element-plus/es")["ElSkeleton"];
+    ElSkeletonItem: typeof import("element-plus/es")["ElSkeletonItem"];
+    ElTooltip: typeof import("element-plus/es")["ElTooltip"];
+    GameDetail: typeof import("./src/components/recordList/gameDetail/index.vue")["default"];
+    GameItem: typeof import("./src/components/GameItem.vue")["default"];
+    GameList: typeof import("./src/components/GameList.vue")["default"];
+    GmailItem: typeof import("./src/components/recordList/gameList/gmailItem.vue")["default"];
+    Pagination: typeof import("./src/components/pagination/index.vue")["default"];
+    PlayerItem: typeof import("./src/components/recordList/gameDetail/playerItem.vue")["default"];
+    Querry: typeof import("./src/components/Querry.vue")["default"];
+    RecordList: typeof import("./src/components/recordList/index.vue")["default"];
+    RouterLink: typeof import("vue-router")["RouterLink"];
+    RouterView: typeof import("vue-router")["RouterView"];
+    SummonerInfo: typeof import("./src/components/summonerInfo/index.vue")["default"];
+    SvgIcon: typeof import("./src/components/svgIcon/index.vue")["default"];
+    UserInfo: typeof import("./src/components/UserInfo.vue")["default"];
   }
   export interface ComponentCustomProperties {
-    vLoading: typeof import('element-plus/es')['ElLoadingDirective']
+    vLoading: typeof import("element-plus/es")["ElLoadingDirective"];
   }
 }
