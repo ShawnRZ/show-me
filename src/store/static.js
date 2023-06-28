2e6c5956c68ec7f95ea16753a7d36ee0941a7ffe
tygorge <2568452755@qq.com>
2023-06-29 00:54:54 +0800

    静态资源加载失败

--- src/store/static.js
+++ src/store/static.js
@@ -14,21 +14,21 @@ export const useSpellStore = defineStore("spell", {
       if (this.Spell.size === 0) {
         const maxRetryCount = 10;
         let retryCount = 0;
-        while (retryCount < maxRetryCount) {
-          try {
-            this.Spell = await _getSpellUrl();
-            console.log("Spell静态资源获取成功");
-            break;
-          } catch (e) {
-            retryCount++;
-            if (retryCount >= maxRetryCount) {
-              $Message("Spell静态资源获取失败！", e, "error");
-              break;
-            }
-            // 重试之前等待一段时间
-            await new Promise((resolve) => setTimeout(resolve, 1000));
-          }
-        }
+        // while (retryCount < maxRetryCount) {
+        //   try {
+        //     this.Spell = await _getSpellUrl();
+        //     console.log("Spell静态资源获取成功");
+        //     break;
+        //   } catch (e) {
+        //     retryCount++;
+        //     if (retryCount >= maxRetryCount) {
+        //       $Message("Spell静态资源获取失败！", e, "error");
+        //       break;
+        //     }
+        //     // 重试之前等待一段时间
+        //     await new Promise((resolve) => setTimeout(resolve, 1000));
+        //   }
+        // }
       }
       return this.Spell;
     },
@@ -47,21 +47,21 @@ export const usePerkStore = defineStore("perk", {
         // 获取静态资源，失败再次获取，最多重新获取10次
         const maxRetryCount = 10;
         let retryCount = 0;
-        while (retryCount < maxRetryCount) {
-          try {
-            this.Perk = await _getPerkUrl();
-            console.log("Perk静态资源获取成功");
-            break;
-          } catch (e) {
-            retryCount++;
-            if (retryCount >= maxRetryCount) {
-              $Message("Perk静态资源获取失败！", e, "error");
-              break;
-            }
-            // 重试之前等待一段时间
-            await new Promise((resolve) => setTimeout(resolve, 1000));
-          }
-        }
+        // while (retryCount < maxRetryCount) {
+        //   try {
+        //     this.Perk = await _getPerkUrl();
+        //     console.log("Perk静态资源获取成功");
+        //     break;
+        //   } catch (e) {
+        //     retryCount++;
+        //     if (retryCount >= maxRetryCount) {
+        //       $Message("Perk静态资源获取失败！", e, "error");
+        //       break;
+        //     }
+        //     // 重试之前等待一段时间
+        //     await new Promise((resolve) => setTimeout(resolve, 1000));
+        //   }
+        // }
       }
       return this.Perk;
     },
@@ -78,21 +78,21 @@ export const useItemStore = defineStore("item", {
         // 获取静态资源，失败再次获取，最多重新获取10次
         const maxRetryCount = 10;
         let retryCount = 0;
-        while (retryCount < maxRetryCount) {
-          try {
-            this.Item = await _getItemUrl();
-            console.log("Item静态资源获取成功");
-            break;
-          } catch (e) {
-            retryCount++;
-            if (retryCount >= maxRetryCount) {
-              $Message("Item静态资源获取失败！", e, "error");
-              break;
-            }
-            // 重试之前等待一段时间
-            await new Promise((resolve) => setTimeout(resolve, 1000));
-          }
-        }
+        // while (retryCount < maxRetryCount) {
+        //   try {
+        //     this.Item = await _getItemUrl();
+        //     console.log("Item静态资源获取成功");
+        //     break;
+        //   } catch (e) {
+        //     retryCount++;
+        //     if (retryCount >= maxRetryCount) {
+        //       $Message("Item静态资源获取失败！", e, "error");
+        //       break;
+        //     }
+        //     // 重试之前等待一段时间
+        //     await new Promise((resolve) => setTimeout(resolve, 1000));
+        //   }
+        // }
       }
       return this.Item;
     },
